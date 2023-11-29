01e9c7f31eacee3acd5a2768a3f1697d10930da8
Andelson <ledson@Andelsons-iMac-Pro.local>
2023-11-29 15:46:09 -0400

    technologies view added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.set("views", path.join(__dirname, "/views/"));
 app.use(express.static(path.join(__dirname, "/public/"))); 
 
 // Homepage
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   return res.render('homepage', {
     title: "Homepage",
   });
--- src/public/js/getUrl.js
+++ src/public/js/getUrl.js
@@ -1,11 +1,33 @@
-// Obtener la URL actual
+const Homepage = document.getElementById('HomepageLink');
+const AboutPage = document.getElementById('AboutLink');
+const TechnologiesPage = document.getElementById('TechsLink');
+const ProjectsPage = document.getElementById('ProjectsLink');
+const BlogPage = document.getElementById('BlogLink');
+
+// Obtener la URL actual y ultimo parametro
 var urlActual = window.location.href;
-
-// Dividir la URL en partes utilizando '/' como separador
 var partesURL = urlActual.split('/');
-
-// Obtener el último elemento de las partes (puede ser el último parámetro o la última parte de la ruta)
 var ultimoElemento = partesURL[partesURL.length - 1];
 
 console.log('URL actual:', urlActual);
 console.log('Último parámetro o ubicación:', ultimoElemento);
+
+if (ultimoElemento == "") {
+    Homepage.style.textDecoration = "underline";
+}
+
+if (ultimoElemento == "about") {
+    AboutPage.style.textDecoration = "underline";
+}
+
+if (ultimoElemento == "Technologies") {
+    TechnologiesPage.style.textDecoration = "underline";
+}
+
+if (ultimoElemento == "Projects") {
+    ProjectsPage.style.textDecoration = "underline";
+}
+
+if (ultimoElemento == "Blog") {
+    BlogPage.style.textDecoration = "underline";
+}
